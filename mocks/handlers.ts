21b4cd5af29b6809c8058c6b01d4f8c888a76639
Carl Luo <4597409+mdluo@users.noreply.github.com>
2022-09-05 09:37:36 +0800

    test: ts-ignore

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -2,7 +2,11 @@ import { graphql } from 'msw';
 
 export const handlers = [
   // Handles a "Login" mutation
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   graphql.mutation('Login', null),
   // Handles a "GetUserInfo" query
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   graphql.query('GetUserInfo', null),
 ];
